00a4ecb32f5589b55a7c3d3d68045cbbf93d22c2
James Forster <jamesalexforster@gmail.com>
2020-07-06 16:39:32 +0100

    first jasmine test written

---
+++ spec/JavabuzzSpec.js
@@ -0,0 +1,14 @@
+describe('Javabuzz', function() {
+
+  var javabuzz;
+
+  describe('knows when a number is', function() {
+  
+    it('divisible by 3', function() {
+      javabuzz = new Javabuzz();
+      expect(isDivisibleByThree(3)).toBe(true);
+    });
+
+  });
+
+});
